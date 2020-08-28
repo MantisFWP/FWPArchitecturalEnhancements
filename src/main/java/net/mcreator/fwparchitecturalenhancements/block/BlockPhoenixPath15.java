fae5240ee0202bc7d36d036ebbdb48f6771b6bb6
MantisFWP <carte@FWP-PC-1-2019.hitronhub.home>
2020-08-27 21:02:35 -0500

    EuroUpdate1

---
+++ src/main/java/net/mcreator/fwparchitecturalenhancements/block/BlockPhoenixPath15.java
@@ -0,0 +1,53 @@
+
+package net.mcreator.fwparchitecturalenhancements.block;
+
+import net.minecraftforge.fml.relauncher.SideOnly;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.common.registry.GameRegistry;
+import net.minecraftforge.client.model.ModelLoader;
+import net.minecraftforge.client.event.ModelRegistryEvent;
+
+import net.minecraft.item.ItemBlock;
+import net.minecraft.item.Item;
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.Block;
+
+import net.mcreator.fwparchitecturalenhancements.creativetab.TabArchitecturalEnhancements;
+import net.mcreator.fwparchitecturalenhancements.ElementsFwparchitecturalenhancementsMod;
+
+@ElementsFwparchitecturalenhancementsMod.ModElement.Tag
+public class BlockPhoenixPath15 extends ElementsFwparchitecturalenhancementsMod.ModElement {
+	@GameRegistry.ObjectHolder("fwparchitecturalenhancements:phoenix_path_15")
+	public static final Block block = null;
+	public BlockPhoenixPath15(ElementsFwparchitecturalenhancementsMod instance) {
+		super(instance, 769);
+	}
+
+	@Override
+	public void initElements() {
+		elements.blocks.add(() -> new BlockCustom().setRegistryName("phoenix_path_15"));
+		elements.items.add(() -> new ItemBlock(block).setRegistryName(block.getRegistryName()));
+	}
+
+	@SideOnly(Side.CLIENT)
+	@Override
+	public void registerModels(ModelRegistryEvent event) {
+		ModelLoader.setCustomModelResourceLocation(Item.getItemFromBlock(block), 0,
+				new ModelResourceLocation("fwparchitecturalenhancements:phoenix_path_15", "inventory"));
+	}
+	public static class BlockCustom extends Block {
+		public BlockCustom() {
+			super(Material.ROCK);
+			setUnlocalizedName("phoenix_path_15");
+			setSoundType(SoundType.STONE);
+			setHarvestLevel("pickaxe", 1);
+			setHardness(1F);
+			setResistance(10F);
+			setLightLevel(0F);
+			setLightOpacity(255);
+			setCreativeTab(TabArchitecturalEnhancements.tab);
+		}
+	}
+}
