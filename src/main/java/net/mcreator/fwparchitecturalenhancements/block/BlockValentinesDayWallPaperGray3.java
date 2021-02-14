e1f02a3d81d8fb4e7e55b3a9ee63ce2906cd1b77
MantisFWP <carte@DESKTOP-CB62NO7.hitronhub.home>
2021-02-14 14:25:23 -0600

    Valentines Day

---
+++ src/main/java/net/mcreator/fwparchitecturalenhancements/block/BlockValentinesDayWallPaperGray3.java
@@ -0,0 +1,52 @@
+
+package net.mcreator.fwparchitecturalenhancements.block;
+
+import net.minecraftforge.fml.relauncher.SideOnly;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.common.registry.GameRegistry;
+import net.minecraftforge.client.model.ModelLoader;
+import net.minecraftforge.client.event.ModelRegistryEvent;
+
+import net.minecraft.item.ItemBlock;
+import net.minecraft.item.Item;
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.Block;
+
+import net.mcreator.fwparchitecturalenhancements.creativetab.TabArchitecturalEnhancements;
+import net.mcreator.fwparchitecturalenhancements.ElementsFwparchitecturalenhancementsMod;
+
+@ElementsFwparchitecturalenhancementsMod.ModElement.Tag
+public class BlockValentinesDayWallPaperGray3 extends ElementsFwparchitecturalenhancementsMod.ModElement {
+	@GameRegistry.ObjectHolder("fwparchitecturalenhancements:valentines_day_wall_paper_gray_3")
+	public static final Block block = null;
+	public BlockValentinesDayWallPaperGray3(ElementsFwparchitecturalenhancementsMod instance) {
+		super(instance, 1265);
+	}
+
+	@Override
+	public void initElements() {
+		elements.blocks.add(() -> new BlockCustom().setRegistryName("valentines_day_wall_paper_gray_3"));
+		elements.items.add(() -> new ItemBlock(block).setRegistryName(block.getRegistryName()));
+	}
+
+	@SideOnly(Side.CLIENT)
+	@Override
+	public void registerModels(ModelRegistryEvent event) {
+		ModelLoader.setCustomModelResourceLocation(Item.getItemFromBlock(block), 0,
+				new ModelResourceLocation("fwparchitecturalenhancements:valentines_day_wall_paper_gray_3", "inventory"));
+	}
+	public static class BlockCustom extends Block {
+		public BlockCustom() {
+			super(Material.WOOD);
+			setUnlocalizedName("valentines_day_wall_paper_gray_3");
+			setSoundType(SoundType.WOOD);
+			setHardness(1F);
+			setResistance(10F);
+			setLightLevel(0F);
+			setLightOpacity(255);
+			setCreativeTab(TabArchitecturalEnhancements.tab);
+		}
+	}
+}
